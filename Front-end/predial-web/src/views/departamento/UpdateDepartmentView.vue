f2d82dafd03a9654feedf34b283b2bb65bdd5831
elizabethleite <101938881+elizabethleite@users.noreply.github.com>
2023-09-15 15:41:44 -0300

    Criada landing page primeira parte

--- Front-end/predial-web/src/views/departamento/UpdateDepartmentView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-
-</script>
-
-<template>
-    <h1>Editar Departamento</h1>
-</template>
---
+++ Novo2/Front-end/predial-web/src/router/index.ts
@@ -0,0 +1,70 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/login/LoginView.vue'
+import ListDepartmentView from '@/views/departamento/ListDepartmentView.vue'
+import AddDepartmentView from '@/views/departamento/AddDepartmentView.vue'
+import UpdateDepartmentView from '@/views/departamento/UpdateDepartmentView.vue'
+import ListClientViewVue from '@/views/cliente/ListClientView.vue'
+import ListFuncionarioViewVue from '@/views/funcionario/ListFuncionarioView.vue'
+import UpdateFuncionarioViewVue from '@/views/funcionario/UpdateFuncionarioView.vue'
+import AddFuncionarioViewVue from '@/views/funcionario/AddFuncionarioView.vue'
+import LandingPageVue from '@/views/landingPage/LandingPage.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: LoginView,
+      meta: {
+        hideNavBar: true,
+      }
+    },
+    {
+      path: '/home',
+      name: 'landingPage',
+      component: LandingPageVue,
+      meta: {
+        hideNavBar: true,
+      }
+    },
+    
+    {
+      path: '/departamentos',
+      name: 'departamentos',
+      component: ListDepartmentView
+    },
+    {
+      path: '/adicionar-departamento',
+      name: 'adicionar-departamento',
+      component: AddDepartmentView
+    },
+    {
+      path: '/editar-departamento',
+      name: 'editar-departamento',
+      component: UpdateDepartmentView
+    },
+    {
+      path: '/clientes',
+      name: 'clientes',
+      component: ListClientViewVue
+    },
+    {
+      path: '/funcionarios',
+      name: 'funcionario',
+      component: ListFuncionarioViewVue
+    },
+    {
+      path: '/editar-funcionario',
+      name: 'editar-funcionario',
+      component: UpdateFuncionarioViewVue
+    },
+    {
+      path: '/adicionar-funcionario',
+      name: 'adicionar-funcionario',
+      component: AddFuncionarioViewVue
+    }
+  ]
+})
+
+export default router
