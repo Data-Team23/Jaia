c7ab5b385c848927e09acaa73e5e1b77f854c921
Júlia Quitério <83841092+juliaquiterio@users.noreply.github.com>
2023-09-17 14:30:35 -0300

    Add Interface IDepartamentoService

---
+++ Back-end/src/main/java/com/dataTeam/jaia/jaia/service/IDepartamentoService.java
@@ -0,0 +1,9 @@
+package com.dataTeam.jaia.jaia.service;
+
+import com.dataTeam.jaia.jaia.model.Departamento;
+
+public interface IDepartamentoService {
+    
+    public Departamento novoDepartamento(Departamento departamento);
+
+}
