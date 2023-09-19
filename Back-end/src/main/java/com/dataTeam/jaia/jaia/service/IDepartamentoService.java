d5e7e4bd47e93334dd53b9ddab28c40dea0b2344
Júlia Quitério <83841092+juliaquiterio@users.noreply.github.com>
2023-09-19 20:02:31 -0300

    update IDepartamentoService

--- Back-end/src/main/java/com/dataTeam/jaia/jaia/service/IDepartamentoService.java
+++ Back-end/src/main/java/com/dataTeam/jaia/jaia/service/IDepartamentoService.java
@@ -2,17 +2,25 @@ package com.dataTeam.jaia.jaia.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.dataTeam.jaia.jaia.model.Departamento;
 
 public interface IDepartamentoService {
 
     //add departamento
-    public Departamento novoDepartamento(Departamento departamento);
+    ResponseEntity<Departamento> novoDepartamento(Departamento departamento);
 
     //delete departamento
-    boolean deleteDepartamento(Long id);
+    ResponseEntity<String> deleteDepartamento(Long id);
+
+    //delete teste
+    //Departamento deleteDepartamento1 (Long id);
 
     //listar departamento
     List<Departamento> listarDepartamento();
+
+    //update departamento
+    //ResponseEntity<String> updateDepartamento(Long id);
 
 }
