0cc6e6e0d494540b8463da268cf914a6ad8b49bb
Júlia Quitério <83841092+juliaquiterio@users.noreply.github.com>
2023-09-23 13:39:28 -0300

    update departamento

---
+++ Back-end/src/main/java/com/dataTeam/jaia/jaia/controller/DepartamentoController.java
@@ -0,0 +1,49 @@
+package com.dataTeam.jaia.jaia.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dataTeam.jaia.jaia.model.Departamento;
+import com.dataTeam.jaia.jaia.service.IDepartamentoService;
+
+@RestController
+@RequestMapping(value = "/departamentos")
+@CrossOrigin
+public class DepartamentoController {
+
+    @Autowired
+    private IDepartamentoService service;
+
+    // Adicionar departamento
+    @PostMapping
+    public Departamento novoDepartamento(@RequestBody Departamento departamento){
+        return service.novoDepartamento(departamento);
+    }
+
+    // Deletar departamento por ID
+    @GetMapping("/{cod_depart}")
+    public Departamento deleteDepartamento(@PathVariable("cod_depart") Long cod_depart){
+        return service.deleteDepartamento(cod_depart);
+    }
+
+    // Listar todos os departamentos
+    @GetMapping
+    public List<Departamento> buscarTodos(){
+        return service.listarDepartamento();
+    }
+
+    // Atualizar departamento por ID
+    @PutMapping("/{cod_depart}")
+    public Departamento atualizarDepartamento(@PathVariable("cod_depart") Long cod_depart, @RequestBody Departamento atualizarDepartamento) {
+        return service.atualizarDepartamento(cod_depart, atualizarDepartamento);
+    }
+}
---
+++ Back-end/src/main/java/com/dataTeam/jaia/jaia/model/Departamento.java
@@ -0,0 +1,69 @@
+package com.dataTeam.jaia.jaia.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "departamento")
+public class Departamento {
+
+
+//private Funcionario funcionario;
+
+    @Id
+    @Column(name = "cod_depart")
+    private Long cod_depart;
+
+    @Column(name = "nome_depart", unique = true)
+    private String nome_depart;
+
+     //@OneToMany(mappedBy = "departamento")
+    //private List<Funcionario> funcionarios;
+
+
+    public Long getCod_depart() {
+        return cod_depart;
+    }
+
+    public void setCod_depart(Long cod_depart) {
+        this.cod_depart = cod_depart;
+    }
+
+
+    public String getNome_depart() {
+        return nome_depart;
+    }
+
+    public void setNome_depart(String nome_depart) {
+        this.nome_depart = nome_depart;
+    }
+
+    public Departamento(Long cod_depart, String nome_depart){
+        this.cod_depart = cod_depart;
+        this.nome_depart = nome_depart;
+
+    }
+
+
+
+    /*public String getNome_func() {
+        return funcionario.getNome_func();
+    }
+
+
+    public void setNome_func(String nome){
+        funcionario.setNome_func(nome);
+    }*/
+    
+
+}
+
+
+
+
+    
+
+
