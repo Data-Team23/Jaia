fd6d624f14b6f68a34be546081121aa1dbb48dc2
Júlia Quitério <83841092+juliaquiterio@users.noreply.github.com>
2023-10-15 11:26:28 -0300

    requisicao backend

---
+++ Back-end/src/main/java/com/dataTeam/jaia/jaia/controller/RequisicaoController.java
@@ -0,0 +1,74 @@
+package com.dataTeam.jaia.jaia.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dataTeam.jaia.jaia.model.Requisicao;
+import com.dataTeam.jaia.jaia.service.Requisicao.IRequisicaoService;
+
+@RestController
+@RequestMapping(value = "/requisicao")
+@CrossOrigin
+public class RequisicaoController {
+
+    @Autowired
+    private IRequisicaoService service;
+
+    @GetMapping
+    public List<Requisicao> buscarTodasRequisicoes(){
+        return service.buscarTodasRequisicoes();
+    }
+
+    @PostMapping
+    public Requisicao novaRequisicao(@RequestBody Requisicao requisicao){
+        return service.novaRequisicao(requisicao);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> buscarPorId(@PathVariable Long id) {
+    try {
+        Requisicao requisicaoEncontrada = service.buscarPorId(id);
+        return ResponseEntity.ok(requisicaoEncontrada);
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("Erro ao buscar requisicao com id informado.");
+    }
+}
+
+    @DeleteMapping("/{id}")
+    public Requisicao deletarPorId(@PathVariable Long id){
+        return service.deletarPorId(id);
+    }
+
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<?> atualizarPorId(@PathVariable Long id,@RequestBody Requisicao requisicaoAtualizada) {
+        try {
+            Requisicao requisicaoAtualizadaResult = service.atualizarPorId(id, requisicaoAtualizada);
+            return ResponseEntity.ok(requisicaoAtualizadaResult);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno ao atualizar a requisição");
+        }
+
+
+
+
+    }
+
+
+    
+}
