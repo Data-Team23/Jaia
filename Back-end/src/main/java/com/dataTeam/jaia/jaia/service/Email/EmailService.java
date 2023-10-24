8b5081ae272773634b94d23d45444b31e6f4a1a3
Júlia Quitério <83841092+juliaquiterio@users.noreply.github.com>
2023-10-23 22:33:05 -0300

    stand by email

---
+++ Back-end/src/main/java/com/dataTeam/jaia/jaia/service/Email/EmailService.java
@@ -0,0 +1,50 @@
+package com.dataTeam.jaia.jaia.service.Email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import com.dataTeam.jaia.jaia.model.Cliente;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    @Value("${support.mail}")
+    private String supportMail;
+
+
+    public void enviarEmailCliente(String assunto, String email, String conteudo) throws MessagingException{
+
+        MimeMessage mail = mailSender.createMimeMessage();
+
+        MimeMessageHelper mensagem = new MimeMessageHelper(mail);
+        mensagem.setAssunto(assunto);
+        mensagem.setEmail(email);
+        mensagem.setFrom(supportMail);
+        mensagem.setTo(email);
+
+        mailSender.send(mail);
+
+    }
+
+
+    public String getContentMailCertificate(Cliente String nome, String senha, String cnpj){
+        return "<p>Ol&aacute;," + nome + "! bem vindo(a) ao Predial!</p>" +
+                "<p>&nbsp;</p>" +
+                "<p>Seu Login est&aacute; dispon&iacute;vel abaixo:<br /><br /></p>" +
+                "<p>Login:</p>" +
+                "<p>CPF: </p>" + cnpj +
+                "<p>Senha: </p>" + senha;
+    }
+    
+    
+}
